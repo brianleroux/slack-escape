f0b6aa2da04e0e62edbfc150aabbbc5d44e20900
brianleroux <b@brian.io>
2016-06-17 13:35:38 -0700

    hbd

---
+++ index.js
@@ -0,0 +1,37 @@
+var Emoji = require('@smallwins/js-emoji')
+var emoji = new Emoji
+
+function escapeHTML(txt) {
+  var lessThan = /(&lt;)/g
+  var greaterThan = /(&gt;)/g
+  var ampersand = /(&amp;)/g
+  return txt.replace(lessThan, '<').replace(greaterThan, '>').replace(ampersand, '&')
+}
+
+function escapeEmoji(txt) {
+  var maybeEmojiIndex = 0
+  var slackIndex = 3
+  // create an insane collections of regexes dynamically on a private member 
+  // what could possibly go wrong
+  var insane = [] 
+  Object.keys(emoji.data).forEach(function(k) {
+    var unicodeValue = emoji.data[k][maybeEmojiIndex][0] // because > 0 === fallbacks
+    var slackSyntaxes = emoji.data[k][slackIndex]
+    slackSyntaxes.forEach(function(sin) {
+      //  console.log(sin)
+      if (sin === '+1') {
+        sin = '\\+1'
+      }
+      insane.push([new RegExp(':' + sin + ':'), unicodeValue])
+    })
+  })
+  // loop thru the insane replacing
+  insane.forEach(function(tuple) {
+    txt = txt.replace(tuple[0], tuple[1])
+  })
+  return txt
+}
+
+module.exports = function escape(txt) {
+  return escapeHTML(escapeEmoji(txt))
+}
---
+++ test.js
@@ -0,0 +1,36 @@
+var escapes = require('.')
+var test = require('tape')
+
+test('ok lets do this', t=> {
+  t.plan(1)
+  t.ok(escapes, 'exists')
+})
+
+test('escape greater than', t=> {
+  t.plan(1)
+  var bad = 'foo &gt; baz'
+  var good = 'foo > baz'
+  t.equal(escapes(bad), good, 'fixed em')
+})
+
+test('escape less than', t=> {
+  t.plan(1)
+  var bad = 'foo &lt; baz'
+  var good = 'foo < baz'
+  t.equal(escapes(bad), good, 'fixed em')
+})
+
+test('escape ampersand', t=> {
+  t.plan(1)
+  var bad = 'foo &amp; baz'
+  var good = 'foo & baz'
+  t.equal(escapes(bad), good, 'fixed em')
+})
+
+test('escape smiley', t=> {
+  t.plan(1)
+  var bad = 'foo :smile: baz'
+  var good = 'foo \uD83D\uDE04 baz'
+  t.equal(escapes(bad), good, 'fixed em')
+  console.log(escapes(bad))
+})
